f06deeb6441840a0b58162bafbbf841b8c815e00
alex <work@xingyongtao.xyz>
2018-06-07 10:58:54 +0800

    add daomanager

---
+++ contract/daomanager.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file
+ *  @copyright defined in LICENSE.txt
+ */
+#include <eosiolib/eosio.hpp>
+#include <string>
+
+using namespace eosio;
+using std::string;
+
+class daomanager: public eosio::contract {
+public:
+	bool create(uint64_t id, account_name owner, string name, string desc);
+	bool updatae_owner(string new_owner);
+	bool update_name(string new_name);
+	bool update_desc(string new_desc);
+	bool remove(uint64_t id);
+
+private:
+	struct dao {
+		uint64_t id;
+		account_name owner;
+		string name;
+		string desc;
+		uint64_t timestamp;
+
+		uint64_t primary_key() const {
+			return id;
+		}
+
+		EOSLIB_SERIALIZE( dao, (id)(owner)(name)(desc)(timestamp) )
+	};
+	typedef eosio::multi_index<N(dao), dao> dao_index;
+};
